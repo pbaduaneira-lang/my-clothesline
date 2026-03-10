497f0e1e4f9dc1cca007d9a528fec8c5a6ee4135
Edvaldo <contato@myclothesline.com>
2026-03-10 13:41:06 -0300

    Fix: Configurando db.js para produção (Render)

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "clothesline",
-    password: "Edju@1016@10",
-    port: 5432
-});
+// Em produção no Render, DATABASE_URL será fornecido automaticamente.
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool(
+    connectionString 
+    ? { connectionString, ssl: { rejectUnauthorized: false } } 
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "clothesline",
+        password: "Edju@1016@10",
+        port: 5432
+    }
+);
 
 module.exports = pool;
